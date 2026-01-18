235ce3fd082e074e989c01dc3e25bf70ae39175b
Youngeun Seol <jeyejong70@naver.com>
2026-01-19 04:25:54 +0900

    Update static file settings in base.py
    Changed STATIC_ROOT to 'staticfiles' and added STORAGES configuration for Whitenoise.

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -208,7 +208,13 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
